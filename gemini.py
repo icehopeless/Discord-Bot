de886deb54ccb46cda6b1bd3f09796cfbdf6d6f4
Gabriel Agostinho <114740870+icehopeless@users.noreply.github.com>
2024-11-07 21:10:50 -0300

    Delete gemini.py

--- gemini.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key='')
-
-model = genai.GenerativeModel('gemini-1.5-flash')
-response = model.generate_content("Qual e a linguagem de programacao mais usada no mundo")
-print(response.text)
-
